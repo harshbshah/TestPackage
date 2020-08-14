e0992f1932336edb6bd18be028ebf1e55b905852
Harsh <harsh.hbs@gmail.com>
2020-08-14 12:57:02 -0400

    Initial commit.

---
+++ Sources/TestPackage/TestPackage.swift
@@ -0,0 +1,13 @@
+public struct TestPackage {
+    var text = "Hello, World!"
+    init()
+    {
+        
+    }
+    public static func getSum(_ l:Int,_ r:Int) -> Int
+    {
+        return l + r
+    }
+}
+
+
